c62b8e70bf9118c585280b89c4678cc99ad9ad44
Muhammad Idrees <midrees097@gmail.com>
2024-11-26 19:35:23 +0500

    Updates

---
+++ _next/static/chunks/app/layout-de4ff3bf60ecc743.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{81419:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,40740,23)),Promise.resolve().then(n.t.bind(n,26667,23)),Promise.resolve().then(n.t.bind(n,48331,23))},26667:()=>{},40740:()=>{},48331:()=>{}},e=>{var s=s=>e(e.s=s);e.O(0,[381,917,114,465,743,358],()=>s(81419)),_N_E=e.O()}]);
