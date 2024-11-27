be084330964287e6e559c7260218bf860cc265ee
Muhammad Idrees <midrees097@gmail.com>
2024-11-27 22:19:20 +0500

    changed add button to light

--- app/components/invoice_data_form.tsx
+++ app/components/invoice_data_form.tsx
@@ -109,6 +109,7 @@ export default function InvoiceDataForm() {
                 key: randomId(),
               })
             }
+            variant="light"
           >
             Add item
           </Button>
